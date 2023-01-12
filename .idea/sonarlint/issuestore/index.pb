68af110e401dc6240cd3461ce71e2e2186798fff
andrejosadcuk <Rudyst.1995@gmail.com>
2023-01-12 21:52:50 +0200

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+n
+>src/main/java/ru/alishev/springcourse/config/SpringConfig.java,3/4/345af7db5ffef9651848cc9630cb2316ab18bb19
---
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,8 @@
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PersonDAO {
+}
